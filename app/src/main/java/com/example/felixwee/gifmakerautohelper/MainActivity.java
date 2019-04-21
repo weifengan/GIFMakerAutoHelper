34275490d62ceb30cfbb0c9ad1a997ed69ed04f1
weifengan <weifengan@163.com>
2019-04-22 03:07:08 +0800

    应用内添加跳转权限设置和能跳转无障碍功

--- app/src/main/java/com/example/felixwee/gifmakerautohelper/MainActivity.java
+++ app/src/main/java/com/example/felixwee/gifmakerautohelper/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.felixwee.gifmakerautohelper;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnAlert=(Button)findViewById(R.id.btnSetAlertWindow);
+        btnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())));
+
+
+            }
+        });
+
+        Button btnAcc=(Button)findViewById(R.id.btnOpenGifMakerHelper);
+        btnAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                   startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+                }
+        });
 
     }
+
+
+
 }
